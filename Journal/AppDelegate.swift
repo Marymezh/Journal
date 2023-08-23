d9bbfca96c3d6cb1db9995afc31ca4d85a802ad5
Maria Mezhova <maria.mezhova@yahoo.com>
2023-08-23 13:56:42 +0700

    setting up fake visits

--- Journal/AppDelegate.swift
+++ Journal/AppDelegate.swift
@@ -36,12 +36,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   let center = UNUserNotificationCenter.current()
   let locationManager = CLLocationManager()
+  static let geoCoder = CLGeocoder()
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     let rayWenderlichColor = UIColor(red: 0/255, green: 104/255, blue: 55/255, alpha: 1)
     UITabBar.appearance().tintColor = rayWenderlichColor
     
     locationManager.requestAlwaysAuthorization()
+    
+    locationManager.startMonitoringVisits()
+    locationManager.delegate = self
     
     center.requestAuthorization (options: [.alert, .sound]) { granted, error in
       if let error = error {
@@ -50,6 +54,62 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     return true
+  }
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+    let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+    AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+      if let place = placemarks?.first {
+        let description = "\(place)"
+        
+        self.newVisitReceived(visit, description: description)
+      }
+    }
+  }
+  
+  func newVisitReceived(_ visit: CLVisit, description: String) {
+    let location = Location(visit: visit, descriptionString: description)
+    
+    let content = UNMutableNotificationContent()
+    content.title = "New Journal entry"
+    content.body = location.description
+    content.sound = .default
+    
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+    
+    center.add(request)
+  }
+}
+
+final class FakeVisit: CLVisit {
+  private let myCoordinates: CLLocationCoordinate2D
+  private let myArrivalDate: Date
+  private let myDepartureDate: Date
+  
+  override var coordinate: CLLocationCoordinate2D {
+    return myCoordinates
+  }
+  
+  override var arrivalDate: Date {
+    return myArrivalDate
+  }
+  
+  override var departureDate: Date {
+    return myDepartureDate
+  }
+  
+  init(coordinates: CLLocationCoordinate2D, arrivalDate: Date, departureDate: Date) {
+    myCoordinates = coordinates
+    myArrivalDate = arrivalDate
+    myDepartureDate = departureDate
+    super.init()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
 }
 
